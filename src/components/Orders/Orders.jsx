bc056d2e186b738c3235db1c6379354cf66ea45e
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-04-06 23:20:32 +0600

    Display Review Items products on the Shopping cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
-
+import ReviewItem from "../ReviewItem/ReviewItem";
+import './Orders.css'
 const Orders = () => {
     const cart = useLoaderData();
   return (
     <div className="shop-container">
-      <div className="products-container">
-        <h3>Orders page : {cart.length}</h3>
+      <div className="review-container">
+        {
+          cart.map(product => <ReviewItem key={product.id}
+          product={product}></ReviewItem>)
+        }
       </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
